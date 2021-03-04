5955169feddf6ab60ec64a4b090da193c78de1c0
Joseph Micla <josephmicla@gmail.com>
2021-03-03 23:37:40 -0800

    divided newsletter into 2 flex containers

--- views/home.js
+++ views/home.js
@@ -7,14 +7,16 @@ module.exports = () => {
         <div class="newsletter__img">
             <img src="https://images.unsplash.com/photo-1541298645675-6cc8e127934d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80">
         </div>
-        <div class="newsletter__description">
-            <h2>Do you like stories?</h2>
-            <p>Sign up for my monthly newsletter, and be the first to hear original stories!</p>
+        <div class="newsletter__text">
+            <div class="newsletter__description">
+                <h2>Do you like stories?</h2>
+                <p>Sign up for my monthly newsletter, and be the first to hear original stories!</p>
+            </div>
+            <form class="newsletter__form" method="POST" action="/">
+                <input type="text" name="email" id="email" placeholder="Enter your e-mail address"/>
+                <input type="submit"/>
+            </form>
         </div>
-        <form class="newsletter__form" method="POST" action="/">
-            <input type="text" name="email" id="email" placeholder="Enter your e-mail address"/>
-            <input type="submit"/>
-        </form>
     </div>
     `
     
