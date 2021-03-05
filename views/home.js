114382f6669568bddc66d17cb79cffdb088441e7
Joseph Micla <josephmicla@gmail.com>
2021-03-04 23:30:45 -0800

    finished styling newsletter sign up

--- views/home.js
+++ views/home.js
@@ -9,12 +9,12 @@ module.exports = () => {
         </div>
         <div class="newsletter__text">
             <div class="newsletter__description">
-                <h2>Do you like stories?</h2>
-                <p>Sign up for my monthly newsletter, and be the first to hear original stories!</p>
+                <h2>Ya Like Jazz?</h2>
+                <p>Sign up for my monthly newsletter, and be the first to hear original and definitely not click-bait titled posts!</p>
             </div>
             <form class="newsletter__form" method="POST" action="/">
-                <input type="text" name="email" id="email" placeholder="Enter your e-mail address"/>
-                <input type="submit"/>
+                <input class="newsletter__form--email" type="text" name="email" id="email" placeholder="Enter your e-mail address"/>
+                <button type="submit" class="newsletter__form--submit">Spam Me!</button>
             </form>
         </div>
     </div>
