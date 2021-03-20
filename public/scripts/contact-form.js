b5269ec07941b7dc7b2e7eff80de9a59638fd00e
Joseph Micla <josephmicla@gmail.com>
2021-03-20 16:52:20 -0700

    refactor, and renamed variables in contact-form.js

--- public/scripts/contact-form.js
+++ public/scripts/contact-form.js
@@ -1,15 +1,13 @@
-document.querySelectorAll('input').forEach(elem => {
-    const label = elem.nextElementSibling;
+document.querySelectorAll('input').forEach(input => {
+    const label = input.nextElementSibling;
 
-    
-
-    elem.addEventListener('focus', () => {
+    input.addEventListener('focus', () => {
         label.classList.remove('label__text--blurred');
         label.classList.add('label__text--focused');
     });
 
-    elem.addEventListener('blur', () => {
-        if(!elem.value) {
+    input.addEventListener('blur', () => {
+        if(!input.value) {
             label.classList.remove('label__text--focused');
             label.classList.add('label__text--blurred');
         }
