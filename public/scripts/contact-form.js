6d57845ff99ed840b055901f5f99c6cfa8ced368
Joseph Micla <josephmicla@gmail.com>
2021-03-20 16:44:13 -0700

    added transition to input focus

--- public/scripts/contact-form.js
+++ public/scripts/contact-form.js
@@ -1,13 +1,17 @@
 document.querySelectorAll('input').forEach(elem => {
     const label = elem.nextElementSibling;
 
+    
+
     elem.addEventListener('focus', () => {
         label.classList.remove('label__text--blurred');
         label.classList.add('label__text--focused');
     });
 
     elem.addEventListener('blur', () => {
-        label.classList.remove('label__text--focused');
-        label.classList.add('label__text--blurred');
+        if(!elem.value) {
+            label.classList.remove('label__text--focused');
+            label.classList.add('label__text--blurred');
+        }
     });
 });
