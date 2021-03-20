97a1b20a85f9164a9797955a9493ee0276a40d23
Joseph Micla <josephmicla@gmail.com>
2021-03-20 16:02:34 -0700

    added script for form animation. placeholder moves depending on input focus

---
+++ public/scripts/contact-form.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('input').forEach(elem => {
+    const label = elem.nextElementSibling;
+
+    elem.addEventListener('focus', () => {
+        label.classList.remove('label__text--blurred');
+        label.classList.add('label__text--focused');
+    });
+
+    elem.addEventListener('blur', () => {
+        label.classList.remove('label__text--focused');
+        label.classList.add('label__text--blurred');
+    });
+});
