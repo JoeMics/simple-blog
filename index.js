c2db3118d1cce6aa9fd9c2a04d656a8a2d48a184
Joseph Micla <josephmicla@gmail.com>
2021-03-21 16:03:07 -0700

    added procfile and listens to env port

--- index.js
+++ index.js
@@ -21,4 +21,8 @@ app.get('/', (req, res) => {
     res.send(homePage());
 });
 
-app.listen(3000);
+let port = process.env.PORT;
+if (port ==null || port == '') {
+    port = 3000;
+}
+app.listen(port);
